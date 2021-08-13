b144d4f9670678c347c2405c0fb9c0c5b2f7bc94
justjavac <justjavac@gmail.com>
2021-08-13 20:10:16 +0800

    fix assert type

--- test.ts
+++ test.ts
@@ -1,43 +1,53 @@
+import { assert } from "https://deno.land/std@0.103.0/testing/asserts.ts";
 import * as ffi from "./mod.ts";
 
-function assertType<S extends string>(expected: ffi.CFuntion<S>) {
-  return expected;
+function assertType<S extends string, T extends ffi.CFuntion<S>>() {
+  return assert(true);
 }
 
-assertType<"int add(int a, int b)">(["add", ["i32", "i32"], "i32"]);
-assertType<" int add(int, int)">(["add", ["i32", "i32"], "i32"]);
-assertType<"int  add(int, int)">(["add", ["i32", "i32"], "i32"]);
-assertType<"int add( int, int)">(["add", ["i32", "i32"], "i32"]);
-assertType<"int add(int,  int)">(["add", ["i32", "i32"], "i32"]);
-assertType<"int add(int, int) ">(["add", ["i32", "i32"], "i32"]);
-assertType<"int add(int, int )">(["add", ["i32", "i32"], "i32"]);
-assertType<" int add( int, int )">(["add", ["i32", "i32"], "i32"]);
+Deno.test("int add(int a, int b)", (): void => {
+  assertType<"int add(int a, int b)", ["add", ["i32", "i32"], "i32"]>();
+  assertType<" int add(int, int)", ["add", ["i32", "i32"], "i32"]>();
+  assertType<"int  add(int, int)", ["add", ["i32", "i32"], "i32"]>();
+  assertType<"int add( int, int)", ["add", ["i32", "i32"], "i32"]>();
+  assertType<"int add(int,  int)", ["add", ["i32", "i32"], "i32"]>();
+  assertType<"int add(int, int) ", ["add", ["i32", "i32"], "i32"]>();
+  assertType<"int add(int, int )", ["add", ["i32", "i32"], "i32"]>();
+  assertType<" int add( int, int )", ["add", ["i32", "i32"], "i32"]>();
+});
 
-assertType<"int abs(int a)">(["abs", ["i32"], "i32"]);
-assertType<"int abs(int)">(["abs", ["i32"], "i32"]);
-assertType<" int abs(int a)">(["abs", ["i32"], "i32"]);
-assertType<"int  abs(int a)">(["abs", ["i32"], "i32"]);
-assertType<"int abs(  int a)">(["abs", ["i32"], "i32"]);
-assertType<"int abs (int a )">(["abs", ["i32"], "i32"]);
-assertType<" int  abs(  int a  )">(["abs", ["i32"], "i32"]);
+Deno.test("int abs(int a)", (): void => {
+  assertType<"int abs(int a)", ["abs", ["i32"], "i32"]>();
+  assertType<"int abs(int)", ["abs", ["i32"], "i32"]>();
+  assertType<" int abs(int a)", ["abs", ["i32"], "i32"]>();
+  assertType<"int  abs(int a)", ["abs", ["i32"], "i32"]>();
+  assertType<"int abs(  int a)", ["abs", ["i32"], "i32"]>();
+  assertType<"int abs (int a )", ["abs", ["i32"], "i32"]>();
+  assertType<" int  abs(  int a  )", ["abs", ["i32"], "i32"]>();
+});
 
-assertType<"int foo()">(["foo", [], "i32"]);
-assertType<" int foo()">(["foo", [], "i32"]);
-assertType<"int  foo()">(["foo", [], "i32"]);
-assertType<" int  foo()">(["foo", [], "i32"]);
-assertType<"  int    foo(    )">(["foo", [], "i32"]);
+Deno.test("int foo()", (): void => {
+  assertType<"int foo()", ["foo", [], "i32"]>();
+  assertType<" int foo()", ["foo", [], "i32"]>();
+  assertType<"int  foo()", ["foo", [], "i32"]>();
+  assertType<" int  foo()", ["foo", [], "i32"]>();
+  assertType<"  int    foo(    )", ["foo", [], "i32"]>();
+});
 
-assertType<"void foo()">(["foo", [], "void"]);
-assertType<" void foo()">(["foo", [], "void"]);
-assertType<"void  foo()">(["foo", [], "void"]);
-assertType<"void foo() ">(["foo", [], "void"]);
-assertType<"void foo(   )">(["foo", [], "void"]);
-assertType<"   void    foo(   )   ">(["foo", [], "void"]);
+Deno.test("void foo()", (): void => {
+  assertType<"void foo()", ["foo", [], "void"]>();
+  assertType<" void foo()", ["foo", [], "void"]>();
+  assertType<"void  foo()", ["foo", [], "void"]>();
+  assertType<"void foo() ", ["foo", [], "void"]>();
+  assertType<"void foo(   )", ["foo", [], "void"]>();
+  assertType<"   void    foo(   )   ", ["foo", [], "void"]>();
+});
 
-const nan: never = null as never;
-assertType<"">(nan);
-assertType<"  ">(nan);
-assertType<"int add">(nan);
-assertType<"int add(">(nan);
-assertType<"int add)">(nan);
-assertType<"add()">(nan);
+Deno.test("never", (): void => {
+  assertType<"", never>();
+  assertType<"  ", never>();
+  assertType<"int add", never>();
+  assertType<"int add(", never>();
+  assertType<"int add)", never>();
+  assertType<"add()", never>();
+});
