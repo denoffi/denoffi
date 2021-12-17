acb2b233afb8fb7edef0759d614c4fe47893dee3
迷渡 <justjavac@gmail.com>
2021-12-17 14:23:48 +0800

    add isLittleEndian
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- mod.ts
+++ mod.ts
@@ -15,3 +15,21 @@ export function ptr2cstr(ptr: Deno.UnsafePointer): string {
 export function ptr2cstr_unchecked(ptr: Deno.UnsafePointer): string {
   return new Deno.UnsafePointerView(ptr).getCString();
 }
+
+/**
+ * Returns a boolean indicating if an environment is little endian.
+ */
+export function isLittleEndian(): boolean {
+  /*
+	* Set the uint16 view to a value having distinguishable lower and higher order words.
+	*
+	* 4660 => 0x1234 => 0x12 0x34 => '00010010 00110100' => (0x12,0x34) == (18,52)
+	*/
+  const uint16 = Uint16Array.of(0x1234);
+
+  // Create a uint8 view on top of the uint16 buffer:
+  const uint8view = new DataView(uint16.buffer);
+
+  // If little endian, the least significant byte will be first...
+  return (uint8view.getUint8(0) === 0x34);
+}
