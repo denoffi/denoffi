7cbb5471ef9a13e8796f1c256f4db5eefa126627
迷渡 <justjavac@gmail.com>
2023-07-17 10:37:37 +0800

    use 'Deno.PointerValue

--- types_test.ts
+++ types_test.ts
@@ -1,4 +1,4 @@
-import { assert } from "https://deno.land/std@0.103.0/testing/asserts.ts";
+import { assert } from "https://deno.land/std@0.194.0/testing/asserts.ts";
 import * as ffi from "./mod.ts";
 
 function assertType<
